ad7703bf337248c2f352ac85acfc8ce9c26830b4
Zipexpo <nvtngan@hcmus.edu.vn>
2024-12-11 11:09:47 +0700

    change interface

--- src/components/CollectionEditor.jsx
+++ src/components/CollectionEditor.jsx
@@ -3,21 +3,29 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import schema from "@/lib/schema";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
+import { Button } from "./ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { Label } from "./ui/label";
+import { Checkbox } from "./ui/checkbox";
+import { question_type } from "@/models/utils";
+import "./collectionEditor.scss"
+import { Delete, Plus, Trash } from "lucide-react";
 
 export default function CollectionEditor({id}){
     const [existingData, setExistingData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const {
-        register,
-        handleSubmit,
-        setValue,
-        formState: { errors },
-      } = useForm({
+    const form = useForm({
         resolver: zodResolver(schema), // Use Zod for validation
       });
-
+    const { fields, append, remove } = useFieldArray({
+        control:form.control,
+        name: "questions", // This tells useFieldArray to work with the questions field
+    });
+    
     useEffect(()=>{
         const fetchData = async () => {
             try {
@@ -28,8 +36,9 @@ export default function CollectionEditor({id}){
               const {collection} = await response.json();
               setExistingData(collection);
               // Set default values in the form
-              Object.entries(collection).forEach(([key, value]) => {
-                setValue(key, value);
+              form.setValue("label", collection.label);
+              collection.questions.forEach((question) => {
+                append(question); // Append each question to the form
               });
               setLoading(false);
             } catch (err) {
@@ -39,16 +48,15 @@ export default function CollectionEditor({id}){
           };
       
           fetchData();
-    },[id, setValue])
-    const onSubmit = async (e) => {
-        e.preventDefault();
+    },[id, form.setValue, append])
+    const onSubmit = async (data) => {
     
         const response = await fetch(`/api/collection/${id}`, {
           method: "PUT",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ label }),
+          body: JSON.stringify(data),
         });
     
         if (response.ok) {
@@ -60,88 +68,111 @@ export default function CollectionEditor({id}){
       if (loading) return <p>Loading...</p>;
       if (error) return <p>Error: {error}</p>;
       return (
-        <div>
+        <div className="m-auto">
           <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Edit Collection</h1>
-          <form onSubmit={handleSubmit} className="flex flex-col w-full p-3">
-            {/* Label */}
-            <div>
-            <label htmlFor="label">Label</label>
-            <input id="label" {...register("label")} />
-            {errors.label && <p className="error">{errors.label.message}</p>}
-            </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit} className="flex flex-col w-full p-3 gap-3">
+                {/* Label */}
+                <FormField
+                    control={form.control}
+                    name="label"
+                    render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>Label</FormLabel>
+                        <FormControl>
+                            <Input placeholder="YOUR LABEL" {...field} />
+                        </FormControl>
+                        <FormDescription>
+                            This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                />
 
-            {/* Questions */}
-            <div>
-            <label>Questions</label>
-            <ul>
-                {(existingData.questions??[]).map((_, index) => (
-                <li key={index}>
-                    <div>
-                    <label htmlFor={`questions.${index}.question_header`}>
-                        Question Header
-                    </label>
-                    <input
+                {/* Questions */}
+                <div className="flex flex-col w-full gap-5">
+                    <div className="flex justify-between">
+                        <label>Questions</label>
+                        <Button type="button" onClick={()=>append({question_header: "new question",
+                        question_type:0,
+                        question_required:false,
+                        question_answer: []})}><Plus/> New</Button>
+                    </div>
+                {fields.map((item, index) => (
+                    <div key={item.id} className="question-field">
+                        <div className="flex justify-between">
+                            <h3>Question {index + 1}</h3>
+                            {/* Remove Question Button */}
+                            <Button className="bg-red-700 hover:bg-red-500" type="button" onClick={() => remove(index)}>
+                                <Trash/>
+                            </Button>
+                        </div>
+                    <FormField name={`questions.${index}.question_header`} control={form.control}
+                    render={({field})=><FormItem>
+                    <Label htmlFor={`questions.${index}.question_header`}>Question Header</Label>
+                    <FormControl>
+                        <Input
                         id={`questions.${index}.question_header`}
-                        {...register(`questions.${index}.question_header`)}
+                        {...field}
+                        />
+                    </FormControl>
+                    </FormItem>}/>
+
+                    <FormField name={`questions.${index}.question_type`}
+                        control={form.control}
+                        render={({field})=><FormItem>
+                        <Label htmlFor={`questions.${index}.question_type`}>Question Type</Label>
+                        
+                            <Select
+                            id={`questions.${index}.question_type`}
+                            {...field}
+                            onValueChange={field.onChange} defaultValue={field.value}
+                            >
+                                <FormControl>
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Select a Question type" />
+                                </SelectTrigger>
+
+                                </FormControl>
+                                <SelectContent>
+                                    {question_type.map(q=> <SelectItem key={q} value={q}>{q}</SelectItem>)}
+                                </SelectContent>
+                            </Select>
+                        
+                        </FormItem>}
                     />
-                    {errors.questions?.[index]?.question_header && (
-                        <p className="error">
-                        {errors.questions[index].question_header.message}
-                        </p>
-                    )}
-                    </div>
 
-                    <div>
-                    <label htmlFor={`questions.${index}.question_type`}>
-                        Question Type
-                    </label>
-                    <select
-                        id={`questions.${index}.question_type`}
-                        {...register(`questions.${index}.question_type`)}
-                    >
-                        <option value="type1">Type 1</option>
-                        <option value="type2">Type 2</option>
-                        <option value="type3">Type 3</option>
-                    </select>
-                    {errors.questions?.[index]?.question_type && (
-                        <p className="error">
-                        {errors.questions[index].question_type.message}
-                        </p>
-                    )}
-                    </div>
-
-                    <div>
-                    <label htmlFor={`questions.${index}.question_required`}>
-                        Question Required
-                    </label>
-                    <input
-                        type="checkbox"
+                    <FormField name={`questions.${index}.question_required`}
+                    control={form.control}
+                    render={({field})=><FormItem>
+                    <Label htmlFor={`questions.${index}.question_required`}>Required</Label>
+                    <FormControl>
+                        <Checkbox
                         id={`questions.${index}.question_required`}
-                        {...register(`questions.${index}.question_required`)}
-                    />
-                    </div>
+                        {...field}
+                        />
+                    </FormControl>
+                    </FormItem>}/>
 
-                    <div>
-                    <label htmlFor={`questions.${index}.question_answer`}>
-                        Question Answer
-                    </label>
-                    <input
+                    <FormField name={`questions.${index}.question_answer`}
+                    control={form.control}
+                    render={({field})=><FormItem>
+                    <Label htmlFor={`questions.${index}.question_answer`}>Question Answer</Label>
+                    <FormControl>
+                        <Input
                         id={`questions.${index}.question_answer`}
-                        {...register(`questions.${index}.question_answer.0`)}
-                    />
-                    {errors.questions?.[index]?.question_answer && (
-                        <p className="error">
-                        {errors.questions[index].question_answer.message}
-                        </p>
-                    )}
+                        {...field}
+                        />
+                    </FormControl>
+                    </FormItem>}/>
                     </div>
-                </li>
                 ))}
-            </ul>
-            </div>
+                </div>
 
-            <button type="submit">Save</button>
-          </form>
+                <button type="submit">Save</button>
+            </form>
+        </Form>
         </div>
       );
 }
